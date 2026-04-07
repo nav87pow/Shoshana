a4b46f5fe49ed5165fed564ce3d53b597324dca5
nav87pow <nav87pow@gmail.com>
2026-04-08 01:33:11 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll("section");
+const aside = document.getElementById("scrollText");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const text = entry.target.getAttribute("data-text");
+        if (text) {
+          aside.textContent = text;
+        }
+      }
+    });
+  },
+  {
+    threshold: 0.5, // מתי נחשב "עברנו"
+  }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
